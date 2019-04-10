5d849812a29f9693701bc608566a97f6914d6495
Vitaliy D <vitalik.domnin@gmail.com>
2019-04-11 03:17:03 +0400

    fix notes for 4 checkpoint

--- src/index-even.js
+++ src/index-even.js
@@ -1,42 +1,33 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  const greeting = () => console.log('Welcome to the Brain Games!');
-  greeting();
+  console.log('Welcome to the Brain Games!');
 
-  const ruleBrainEven = () => console.log('Answer "yes" if number even otherwise answer "no". \n');
-  ruleBrainEven();
+  console.log('Answer "yes" if number even otherwise answer "no". \n');
 
   const userName = readlineSync.question('May I have your name?: ');
 
-  const greetingName = () => console.log(`Hello, ${userName}! \n`);
-  greetingName();
+  console.log(`Hello, ${userName}! \n`);
 
   const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-  const questionBrainEven = () => {
-    const num = randomNumber(1, 1000);
-    console.log(`Question: ${num}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (num % 2 === 0 && answer === 'yes') {
-      return console.log('Correct!');
-    }
-    if (num % 2 !== 0 && answer === 'no') {
-      return console.log('Correct!');
-    }
-    return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${userName}`);
-  };
+  const isEven = num => num % 2 === 0;
 
-  const nextLap = () => {
-    for (let i = 0; i < 4;) {
-      if (i === 3) {
-        return console.log(`Congratulations, ${userName}`);
-      }
-      if (questionBrainEven(randomNumber(1, 1000))) { // Не знаю как указать условие, ведь функция questionBrainEven возвращает comsole.log  
-        i += 1;
-      }
-      return;
+  const finalRound = 3;
+
+  for (let round = 0; round < finalRound; round += 1) {
+    const number = randomNumber(1, 1000);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (isEven(number) && answer === 'yes') {
+      console.log('Correct!');
+    } else if (!isEven(number) && answer === 'no') {
+      console.log('Correct!');
+    } else if (isEven(number) && answer !== 'yes') {
+      return console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'. \n Let's try again, ${userName}`);
+    } else if (!isEven(number) && answer !== 'no') {
+      return console.log(`${answer} is wrong answer ;(. Correct answer was 'no'. \n Let's try again, ${userName}`);
     }
-  };
-  nextLap();
+  }
+  return console.log(`Congratulations, ${userName}!`);
 };
